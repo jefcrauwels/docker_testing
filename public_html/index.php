40dffa469a564a50d429901a7ac65b32fc0900a4
Crauwels <j.crauwels@accenture.com>
2019-09-18 14:07:17 +0200

    First commit

---
+++ public_html/index.php
@@ -0,0 +1,34 @@
+<h1>Hello Cloudreach!</h1>
+<h4>Attempting MySQL connection from php...</h4>
+<?php
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL successfully!";
+}
+
+$result = mysqli_query($conn,"SELECT * FROM Persons");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['FirstName'] . "</td>";
+echo "<td>" . $row['LastName'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($conn);
+
+?>
