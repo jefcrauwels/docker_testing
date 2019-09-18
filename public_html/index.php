16b12f63b56c33633ff3210b0c5695ed3ff46b17
Crauwels <j.crauwels@accenture.com>
2019-09-18 17:12:04 +0200

    Modify Index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,34 +1,57 @@
-<h1>Hello Cloudreach!</h1>
+<h1>Hello !</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
 $host = 'mysql';
 $user = 'root';
 $pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
+$database = 'myDB';
+$conn = new mysqli($host, $user, $pass, $database);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $sql = "CREATE DATABASE myDB";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully" . "<br>";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+}else {
+    echo "Connected to MySQL successfully!" . "<br>";
+}
+
+$sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "Table MyGuests created successfully" . "<br>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully" . "<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
 } else {
-    echo "Connected to MySQL successfully!";
+    echo "0 results";
 }
-
-$result = mysqli_query($conn,"SELECT * FROM Persons");
-
-echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['FirstName'] . "</td>";
-echo "<td>" . $row['LastName'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($conn);
+$conn->close();
 
 ?>
